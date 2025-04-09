90a90effe924a84670a559e2cdf377a1225f9c1a
laoyao <laoyao0822@qq.com>
2025-04-09 20:02:46 +0800

    begin to nodist evoformer

--- run-test.py
+++ run-test.py
@@ -51,7 +51,7 @@ from diffusionWorker2.misc import params as diffusion_params
 from diffusionWorker2.diffusion_step_vino import diffusion_vino
 DIFFUSION_ONNX=False
 SAVE_ONNX=False
-UseVino=True
+UseVino=False
 
 _HOME_DIR = pathlib.Path(os.environ.get('HOME'))
 DEFAULT_MODEL_DIR = _HOME_DIR / 'models/model_103275239_1'
@@ -252,7 +252,7 @@ class ModelRunner:
         if _CPU_INFERENCE.value:
             print("mkl",torch.backends.mkl.is_available(),"onednn",torch.backends.mkldnn.is_available())
             if not SAVE_ONNX and  not DIFFUSION_ONNX and not UseVino:
-                pass
+                # pass
                 import intel_extension_for_pytorch as ipex
                 # import openvino as ov
                 # print("Applying Intel Extension for PyTorch optimizations...")
@@ -307,7 +307,7 @@ class ModelRunner:
 
         else: # CPU Inference
             if _CPU_AMP_OPT:
-                # with torch.amp.autocast(device_type="cpu", dtype=torch.bfloat16):
+                with torch.amp.autocast(device_type="cpu", dtype=torch.bfloat16):
                     print("Running inference with AMP on CPU...")
                     # self._model=torch.jit.trace(self._model,featurised_example)
                     # result = self._model(featurised_example)
@@ -370,7 +370,7 @@ class ModelRunner:
                                                       )
                         print("diffusion cost time: ", time.time() - time1)
                 # with torch.amp.autocast(device_type="cpu", dtype=torch.bfloat16):
-                    print(positions[0][0])
+                #     print(positions[0][0])
                     result=self._model(featurised_example,embeddings,positions)
 
                     # if torch.allclose(result, target_feat, rtol=1e-5):
